1deb1ab82431977a587e11eb2fcbc0f1b16eb45f
ryan <ledoryan42@gmail.com>
2025-04-19 20:08:30 -0300

    Implementei a paginacao na agenda

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
 from contact.models import Contact
 from django.db.models import Q
 
@@ -8,10 +9,14 @@ from django.db.models import Q
 def index(request):
     contacts = Contact.objects \
         .filter(show=True) \
-        .order_by('-id')[0:20]
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 8)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - '
     }
 
@@ -37,10 +42,15 @@ def search(request):
             Q(email__icontains=search_value) 
          ) \
         .order_by('-id')
+    
+    paginator = Paginator(contacts, 8)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
-        'site_title': 'Search - '
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value
     }
 
     return render(
